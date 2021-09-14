131c8892e20d903e0b6c919d26aa84c34046dba6
Artur <rutrika@gmail.com>
2021-09-13 21:57:36 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,82 @@
+class Calculator {
+    constructor() {
+
+    }
+
+    getCalculations () {
+
+    }
+
+    appendNumber () {
+
+    }
+
+    updateDisplay () {
+        
+    }
+
+    clear () {
+
+    }
+
+    init () {
+        document.getElementById("btn-1").onclick = () => {
+
+        };
+        document.getElementById("btn-2").onclick = () => {
+
+        };
+        document.getElementById("btn-3").onclick = () => {
+
+        };
+        document.getElementById("btn-4").onclick = () => {
+
+        };
+        document.getElementById("btn-5").onclick = () => {
+
+        };
+        document.getElementById("btn-6").onclick = () => {
+
+        };
+        document.getElementById("btn-7").onclick = () => {
+
+        };
+        document.getElementById("btn-8").onclick = () => {
+
+        };
+        document.getElementById("btn-9").onclick = () => {
+
+        };
+        document.getElementById("btn-0").onclick = () => {
+
+        };
+        document.getElementById("btn+").onclick = () => {
+
+        };
+        document.getElementById("btn-").onclick = () => {
+
+        };
+        document.getElementById("btn-x").onclick = () => {
+
+        };
+        document.getElementById("btn/").onclick = () => {
+
+        };
+        document.getElementById("btn.").onclick = () => {
+
+        };
+        document.getElementById("reset").onclick = () => {
+
+        };
+        document.getElementById("red_btn").onclick = () => {
+
+        };
+
+
+        
+        
+
+        
+
+    }
+}
